be72eefc9ad081e36a4a58c7602052fae4c8e9cb
juanpablo491 <juanpablocarmona580@gmail.com>
2022-09-07 17:38:34 -0500

    segundo trabajo

---
+++ python vs/#1.py
@@ -0,0 +1,14 @@
+#manejo de variables en  python 
+from tokenize import Double
+
+
+print("hola a todos")
+a = 2
+print("la variable es ", a)
+b = str("hola jeje")
+print ("la variable es de tipo ", type(b))
+#suma de variables con numeros que el ususario nos proporciona 
+n1 = float(input("ingrese el primer numero: "))
+n2 = int(input("ingrese el segundo numero: "))
+suma = n1 + n2
+print("la suma de los 2 numeros es ", suma)
